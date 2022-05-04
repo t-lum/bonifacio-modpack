8cdeb2a9c5725b060a80b841e514df5dcd91dab3
Sidnei Jr <xplouses@gmail.com>
2022-05-04 11:10:41 -0300

    reajustando dev

--- pax/src/util/flow.nim
+++
@@ -1,9 +0,0 @@
-## Helper Utilities for easier scripting & code flow management.
-
-template returnIf*(assertion: bool): void =
-  ## returns if `assertion` is true
-  if assertion: return
-
-template returnIfNot*(assertion: bool): void =
-  ## returns if `assertion` is false
-  if not assertion: return
