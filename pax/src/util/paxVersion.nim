6a409c0d1fff0f92eaf2be3130e9bac7295d1346
Sidnei Jr <xplouses@gmail.com>
2022-05-04 02:39:36 -0300

    mods teoricamente sem conflitos

--- pax/src/util/paxVersion.nim
+++
@@ -1,5 +0,0 @@
-## Helper file for retrieving pax version from latest tag
-
-import strutils
-
-const currentPaxVersion*: string = staticExec("git describe --tags HEAD").split("-")[0]
--- pax/tests/term/tprompt/tprompt_yn_skip.nim
+++
@@ -1,9 +0,0 @@
-import term/prompt
-
-skipYNSetting = true
-
-doAssert promptYn("True or false", default = true) == true
-doAssert promptYn("Test with y", default = false) == true
-doAssert promptYn("Test with Y", default = false) == true
-doAssert promptYn("Test with n", default = true) == true
-doAssert promptYn("Test with N", default = true) == true
